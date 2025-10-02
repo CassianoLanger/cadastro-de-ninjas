14b52173561655645fc340eca71b35711dca0bd7
cassianolanger <cassianolangerm@gmail.com>
2025-10-02 14:56:35 -0300

    feat: Atualiza o controller com mock de dados

--- src/main/java/com/clanger/cadastroDeNinjas/ninjas/controller/NinjaController.java
+++ src/main/java/com/clanger/cadastroDeNinjas/ninjas/controller/NinjaController.java
@@ -1,15 +1,43 @@
 package com.clanger.cadastroDeNinjas.ninjas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/ninja")
 public class NinjaController {
 
     @GetMapping("/boas-vindas")
     public String boasVindas(){
         return "primeira mensagem nessa rota";
+    }
+
+    // Adicionar ninja(Create)
+    @GetMapping("/new-ninja")
+    public String postNinja(){
+        return "New ninja is on db now";
+    }
+
+    // Mostrar todos os ninjas(Read)
+    @GetMapping("/all")
+    public String getAllNinjas(){
+        return "all";
+    }
+
+    // Procurar ninja por ID(Read)
+    @GetMapping("/get-id")
+    public String getNinjaById(){
+        return "id: " ;
+    }
+
+    //Alterar dados do ninja(Update)
+    @PutMapping("/update-ninja")
+    public String updateNinja(){
+        return "id: 1";
+    }
+
+    //Deletar Ninja(Delete)
+    @DeleteMapping("/delete-ninja")
+    public String deleteNinja(){
+        return "id: 2";
     }
 }
